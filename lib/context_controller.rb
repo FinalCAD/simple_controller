85d6df987640d58180f8a14f575a1009075badb8
Steve Chung <steve.chung7@gmail.com>
2015-10-06 17:53:38 -0400

    initial movement from finalcloud

--- lib/context_controller.rb
+++ lib/context_controller.rb
@@ -1,4 +1,14 @@
+autoload = false
+# autoload = true #uncomment for testing purposes only, not covered by rspec
+
 require "context_controller/version"
+
+if autoload && defined?(Rails)
+  require 'context_controller/engine'
+else
+  require 'context_controller/base'
+end
+
 
 module ContextController
   # Your code goes here...
---
+++ lib/context_controller/base.rb
@@ -0,0 +1,24 @@
+module ContextController
+  class Base
+    attr_reader :context, :action_name
+
+    def call(action_name, context={})
+      setup(action_name, context)
+      public_send @action_name
+    end
+
+    protected
+
+    def setup(action_name, context)
+      @context ||= OpenStruct.new(context)
+      @context.options ||= {}
+      @action_name ||= action_name.to_sym
+    end
+
+    class << self
+      def call(*args)
+        new.call *args
+      end
+    end
+  end
+end
